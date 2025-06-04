e494e30b0485c1dd458f6ffae18a3a5daf77b192
dadobr <dadobr@microsoft.com>
2025-06-05 01:57:26 +0300

    first two databases created: Flower and Genre under Models.

---
+++ Models/Flower.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DreamGarden.Models
+{
+    [Table("Flower")]
+    public class Flower
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? FlowerName { get; set; }
+        [Required]
+
+        public int GenreId { get; set; } //connection with the Genre database
+
+        public Genre Genre; //one flower has only one genre
+    }
+}
---
+++ Models/Genre.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DreamGarden.Models
+{
+    [Table("Genre")]
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string GenreName { get; set; }
+
+        public List<Genre> Flowers { get; set; } //one Genre can have multiple flowers
+
+        
+
+
+    }
+}
